d90cfbe333f4d70e0cc421f73517d2217d7f4f9f
charsyam <charsyam@naver.com>
2021-07-04 22:42:26 +0900

    [ISSUE-17] add pipeline example
    resolve: #17

---
+++ chapter_3/pipeline/main.py
@@ -0,0 +1,33 @@
+from time import perf_counter
+
+import redis
+import sys
+
+
+def pipeline(conn, n):
+    t1_start = perf_counter()
+    pipeline = conn.pipeline()
+    for i in range(n):
+        key = 'PR:%s'%(i)
+        pipeline.set(key, key)
+        if i%1024 == 0:
+            pipeline.execute();
+
+    t1_stop = perf_counter()
+    print("Pipeline: Elapsed time:", t1_stop - t1_start, t1_stop, t1_start) 
+
+
+def no_pipeline(conn,n):
+    t1_start = perf_counter()
+    for i in range(n):
+        key = 'PR:%s'%(i)
+        conn.set(key, key)
+
+    t1_stop = perf_counter()
+    print("No-Pipeline Elapsed time:", t1_stop - t1_start, t1_stop, t1_start) 
+
+
+rconn = redis.StrictRedis(host='192.168.0.102', port=6379, db=0)
+
+pipeline(rconn, int(sys.argv[1]))
+no_pipeline(rconn, int(sys.argv[1]))
